9eab8afd0c8a37ccc1f45935e264ead1fb0616e6
l0rem1psum <sunshine.xuan5960@gmail.com>
2018-10-13 18:15:03 +0800

    Added tools to convert *.mp3 to *.mid and to get *.mid file info.

---
+++ miditools.py
@@ -0,0 +1,30 @@
+import os
+from mido import MidiFile
+
+def convert_mp3_to_midi(audio_name, bpm = 60, smooth = 0.15, minduration = 0.15):
+    cmd = "python2 audio_to_melodia.py " + audio_name + ".wav " + audio_name + ".mid "
+    cmd += str(bpm) + " --smooth " + str(smooth) + " --minduration " + str(0.15)
+    os.system(cmd)
+
+def get_midi_info(audio_name):
+    mid = MidiFile(audio_name + ".mid")
+    msg = mid.tracks[1][1]
+
+    on_off = list()
+    note = list()
+    velocity = list()
+    time = list()
+
+    i = 1
+    while(not msg.is_meta):
+
+        on_off.append(msg.type)
+        note.append(msg.note)
+        velocity.append(msg.velocity)
+        time.append(msg.time)
+
+        i += 1
+        msg = mid.tracks[1][i]
+    
+    return {"on_off": on_off, "note": note, "velocity": velocity, "time": time}
+
